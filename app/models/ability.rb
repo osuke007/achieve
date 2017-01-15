c99e7d148f1db4425584fc5f2a73107d0c0e5e0b
osuke <osuke@TOKC02LQEXNFD57.local>
2017-01-15 22:18:56 +0900

    管理画面アクセスの修正

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-      end
+    if user && user.admin?
+         can :access, :rails_admin   # grant access to rails_admin
+         can :manage, :all           # allow superadmins to do anything
+       end
+      # user ||= User.new # guest user (not logged in)
+      # if user.admin?
+      #   can :manage, :all
+      # else
+      #   can :read, :all
+      # end
   end
 end
