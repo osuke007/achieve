71d79c63b1fea203206a7fa0abd0e357204907bc
osuke <osuke@TOKC02LQEXNFD57.local>
2017-02-13 01:04:01 +0900

    コメントの動的削除と編集リンクを削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,38 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
 
-    # クライアント要求に応じてフォーマットを変更
+    # respond_toは、クライアントからの要求に応じてレスポンスのフォーマットを変更します。
     respond_to do |format|
-      if @comment.save
-        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-      else
-        format.html { render :new }
-      end
-    end
+       if @comment.save
+         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+         format.json { render :show, status: :created, location: @comment }
+         # JS形式でレスポンスを返します。
+         format.js { render :index }
+       else
+         format.html { render :new }
+         format.json { render json: @comment.errors, status: :unprocessable_entity }
+       end
+     end
   end
+
+  def destroy
+      @comment = Comment.find(params[:id])
+      @comment.destroy
+
+      # respond_toは、クライアントからの要求に応じてレスポンスのフォーマットを変更します。
+      respond_to do |format|
+         if @comment.delete
+           format.html { redirect_to blog_path(@blog), notice: 'コメントを削除しました。' }
+           format.json { render :show, status: :created, location: @comment }
+           # JS形式でレスポンスを返します。
+           format.js { render :index }
+         else
+           format.html { render :new }
+           format.json { render json: @comment.errors, status: :unprocessable_entity }
+         end
+       end
+  end
+
 
   private
     # ストロングパラメーター
@@ -21,18 +44,7 @@ class CommentsController < ApplicationController
       params.require(:comment).permit(:blog_id, :content)
     end
 
-  # respond_toは、クライアントからの要求に応じてレスポンスのフォーマットを変更します。
-  respond_to do |format|
-     if @comment.save
-       format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-       format.json { render :show, status: :created, location: @comment }
-       # JS形式でレスポンスを返します。
-       format.js { render :index }
-     else
-       format.html { render :new }
-       format.json { render json: @comment.errors, status: :unprocessable_entity }
-     end
-   end
+
 
 
 end
