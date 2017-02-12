ad055e2fd7f24a8bec6622170cb3533a193295e4
osuke <osuke@TOKC02LQEXNFD57.local>
2017-02-12 22:00:24 +0900

    Ajaxの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: "users/registrations",
