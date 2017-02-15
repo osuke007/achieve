2551517de8c48fc0751c12adebd3781782d56f01
osuke <osuke@TOKC02LQEXNFD57.local>
2017-02-15 20:05:59 +0900

    ユーザー一覧体裁修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -30,7 +30,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
+  #version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
 
